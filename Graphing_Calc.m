3816b2f8b0bf55683a6df494e6f58e1c48c42575
anshipoooo <doctoransh@gmail.com>
2017-09-28 12:55:48 -0700

    UI V1

--- Graphing_Calc.m
+++ Graphing_Calc.m
@@ -1,20 +1,4 @@
 
-% while true
-%% Load Graph
-
-
-% equation = lower(input('\nEnter graph function: ','s'));
-
-% 
-% equation=str2func(equation);
-% 
-% plot(x,equation);
-
-% true_equation = str2func(['@(x)' equation]);
-% tru_equation = str2func(equation);
-% 
-% x_axis = linspace(-10,10);
-% plot(x_axis,tru_equation);
 
 function y= myf(x)
     f=@(x) input('Enter equation in terms of x: ');

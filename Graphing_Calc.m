1454eec87fcc52f39afcfa6e9d1c86ed36606d7d
anshipoooo <doctoransh@gmail.com>
2017-10-08 19:31:26 -0700

    UI V2

--- Graphing_Calc.m
+++ Graphing_Calc.m
@@ -1,12 +1,26 @@
 
 
 function y= myf(x)
-    f=@(x) input('Enter equation in terms of x: ');
+    in=input('Enter equation in terms of x: ','s');
+    div='/';
+    exp='^';
+    mult='*';
+
+    new_in=in;
+
+      new_in=strrep(in,div,'./')
+
+      new_in=strrep(in,exp,'.^');
+
+
+      new_in=strrep(in,mult,'.*');
+
+    new_func=str2func(new_in);
+
+    f=@(x) new_func
     y=f(x);
-    
-    
-    
-    
+
+
+
+
 end
-
-
